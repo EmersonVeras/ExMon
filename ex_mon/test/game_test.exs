eb83725c8c1a4eb02d72c112c74f98dedd8b3e9f
EmersonVeras <emersonverasifce@gmail.com>
2022-06-21 16:21:37 -0300

    ExMon project

---
+++ ex_mon/test/game_test.exs
@@ -0,0 +1,26 @@
+defmodule ExMon.GameTest do
+  use ExUnit.Case
+
+  alias ExMon.{Game, Player}
+
+  describe "start/2" do
+    test "starts the game state" do
+      player = Player.build("Emerson", :chute, :soco, :cura)
+      computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+      assert {:ok, _pid} = Game.start(computer, player)
+    end
+  end
+
+  describe "info/0" do
+    test "return the current game state"do
+      player = Player.build("Emerson", :chute, :soco, :cura)
+      computer = Player.build("Robotinik", :chute, :soco, :cura)
+
+      Game.start(computer, player)
+
+      assert Game.info() == "banana"
+    end
+  end
+
+end
